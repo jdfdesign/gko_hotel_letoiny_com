5dfeb01137f7b2e3f1ae7a780f5dd18bee639e5f
regis bruggheman <regis@joufdesign.com>
2013-04-28 13:00:59 +0200

    bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129095812) do
+ActiveRecord::Schema.define(:version => 20130428103925) do
 
   create_table "accounts", :force => true do |t|
     t.string   "reference",  :limit => 40
@@ -548,6 +548,8 @@ ActiveRecord::Schema.define(:version => 20130129095812) do
     t.datetime "liquid_models_updated_at"
     t.text     "page_types"
     t.text     "mailer_settings"
+    t.text     "stylesheet"
+    t.text     "javascript"
   end
 
   add_index "sites", ["account_id"], :name => "index_sites_on_account_id"
