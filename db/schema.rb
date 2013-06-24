983d8fbbff6c0d6f6183ba68ef7b76f1c2e9f825
regis bruggheman <regis@joufdesign.com>
2013-06-24 14:19:15 +0200

    bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130618093621) do
+ActiveRecord::Schema.define(:version => 20130624120928) do
 
   create_table "accounts", :force => true do |t|
     t.string   "reference",  :limit => 40
@@ -410,7 +410,7 @@ ActiveRecord::Schema.define(:version => 20130618093621) do
   create_table "partners", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.string   "url"
+    t.string   "link"
     t.integer  "site_id"
     t.integer  "section_id"
     t.string   "image_mime_type"
